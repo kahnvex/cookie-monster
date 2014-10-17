bb7daf7e0c5d5dd743fe9a10b95f31f4b4e4b293
kahnjw <jarrod@yola.com>
2014-10-16 17:17:10 -0700

    Add tests and cleanup code

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 var exports = module.exports = function (doc) {
-    if (!doc) doc = {};
-    if (typeof doc === 'string') doc = { cookie: doc };
-    if (doc.cookie === undefined) doc.cookie = '';
+  if (!doc) doc = {};
+  if (typeof doc === 'string') doc = { cookie: doc };
+  if (doc.cookie === undefined) doc.cookie = '';
 
-    var self = {};
-    self.get = function (key) {
-        var splat = doc.cookie.split(/;\s*/);
-        for (var i = 0; i < splat.length; i++) {
-            var ps = splat[i].split('=');
-            var k = unescape(ps[0]);
-            if (k === key) return unescape(ps[1]);
-        }
-        return undefined;
-    };
+  var self = {};
+  self.get = function (key) {
+    var cookiesSplat = doc.cookie.split(/;\s*/);
+    for (var i = 0; i < cookiesSplat.length; i++) {
+      var ps = cookiesSplat[i].split('=');
+      var k = unescape(ps[0]);
+      if (k === key) return unescape(ps[1]);
+    }
+    return undefined;
+  };
 
-    self.set = function (key, value, opts) {
-        if (!opts) opts = {};
-        var s = escape(key) + '=' + escape(value);
-        if (opts.expires) s += '; expires=' + opts.expires;
-        if (opts.path) s += '; path=' + escape(opts.path);
-        if (opts.domain) s += '; domain=' + opts.domain;
-        doc.cookie = s;
-        return s;
-    };
-    return self;
+  self.set = function (key, value, opts) {
+    if (!opts) opts = {};
+    var newCookie = escape(key) + '=' + escape(value);
+
+    if (opts.expires){
+      newCookie += '; expires=' + opts.expires;
+    }
+
+    if (opts.path) {
+      newCookie += '; path=' + escape(opts.path);
+    }
+
+    if (opts.domain) {
+      newCookie += '; domain=' + opts.domain;
+    }
+
+    doc.cookie = newCookie;
+
+    return newCookie;
+  };
+  return self;
 };
 
 if (typeof document !== 'undefined') {
-    var cookie = exports(document);
-    exports.get = cookie.get;
-    exports.set = cookie.set;
+  var cookie = exports(document);
+  exports.get = cookie.get;
+  exports.set = cookie.set;
 }
