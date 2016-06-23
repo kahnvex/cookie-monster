938897c284f0431079de04c798051eceadcf290f
Denis Chernov <twkr@twkrsplace.ru>
2016-06-23 23:40:03 +0300

    Refactored main file

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 'use strict';
 
-exports = module.exports = function (doc) {
-  if (!doc) doc = {};
-  if (typeof doc === 'string') doc = { cookie: doc };
-  if (doc.cookie === undefined) doc.cookie = '';
+var bake = function (doc){
+  if(!doc) doc = {};
+  if(typeof doc === 'string') doc = { cookie: doc };
+  if(doc.cookie === undefined) doc.cookie = '';
 
-  var self = {};
-  self.get = function (key) {
-    var cookiesSplat = doc.cookie.split(/;\s*/);
-    for (var i = 0; i < cookiesSplat.length; i++) {
-      var ps = cookiesSplat[i].split('=');
-      var k = decodeURIComponent(ps[0]);
-      if (k === key) return decodeURIComponent(ps[1]);
+  // Returning object
+  var self = {
+    get: function (key){
+      var cookiesSplat = doc.cookie.split(/;\s*/);
+      for (var i = 0; i < cookiesSplat.length; i++) {
+        var ps = cookiesSplat[i].split('=');
+        var k = decodeURIComponent(ps[0]);
+        if (k === key) return decodeURIComponent(ps[1]);
+      }
+    },
+    set: function (key, value, opts){
+      // Checks before we start
+      if (typeof key !== 'string' || typeof value !== 'string') return false;
+      if (!opts) opts = {};
+
+      // Creating new cookie string
+      var newCookie = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+      if (opts.hasOwnProperty('expires')) newCookie += '; expires=' + opts.expires;
+      if (opts.hasOwnProperty('path')) newCookie += '; path=' + opts.path;
+      if (opts.hasOwnProperty('domain')) newCookie += '; domain=' + opts.domain;
+      if (opts.hasOwnProperty('secure')) newCookie += '; secure';
+
+      doc.cookie = newCookie;
+      return newCookie;
     }
   };
 
-  self.set = function (key, value, opts) {
-    if (!opts) opts = {};
-    var newCookie = encodeURIComponent(key) + '=' + encodeURIComponent(value);
-
-    if (opts.hasOwnProperty('expires')){
-      newCookie += '; expires=' + opts.expires;
-    }
-
-    if (opts.hasOwnProperty('path')) {
-      newCookie += '; path=' + opts.path;
-    }
-
-    if (opts.hasOwnProperty('domain')) {
-      newCookie += '; domain=' + opts.domain;
-    }
-
-    if (opts.hasOwnProperty('secure')) {
-      newCookie += '; secure';
-    }
-
-    doc.cookie = newCookie;
-
-    return newCookie;
-  };
   return self;
 };
 
-if (typeof document !== 'undefined') {
-  var cookie = exports(document);
-  exports.get = cookie.get;
-  exports.set = cookie.set;
-}
+if (typeof document !== 'undefined') module.exports = exports = bake(document);
